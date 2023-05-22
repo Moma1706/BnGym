c169d664c421485e52e85b9c341ba2eed9e490de
Moma1706 <milan.momcilovic582@gmail.com>
2023-05-22 20:50:52 +0200

    datetime zamrznut i login/logout

--- Frontend/src/app/account/login.component.ts
+++ Frontend/src/app/account/login.component.ts
@@ -12,29 +12,26 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 
 export class LoginComponent implements OnInit {
-  
   form: FormGroup;
   loading = false;
   submitted = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
     private router: Router,
     private accountService: AccountService,
-    private alertService: AlertService
-  )   
+    private alertService: AlertService)   
   {
     this.form = formBuilder.group({
-      title: formBuilder.control('initial value', Validators.required)
-  });
+      title: formBuilder.control('', Validators.required)
+    });
   }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
-  });
+    });
   }
   
   get f() { return this.form.controls; }
@@ -44,28 +41,23 @@ export class LoginComponent implements OnInit {
     this.submitted = true;
     this.alertService.clear();
     // stop here if form is invalid
-    if (this.form.invalid) {
+    if (this.form.invalid)
       return;
-    }
   }
 
   login(){
     if(this.form.valid){
       this.loading = true;
       this.accountService.login(this.f['username'].value,this.f['password'].value,)
-      .pipe(first())
-            .subscribe({
-                next: () => {
-                    // get return url from query parameters or default to home page
-                    window.location.reload();
-                },
-                error: (error : HttpErrorResponse) => {
-                  this.alertService.error(error.error.error);
-                  this.loading = false;
-              },
-            });
-            const returnUrl ='/home';
-            this.router.navigateByUrl(returnUrl);
+      .pipe(first()).subscribe({
+        next: () => {
+          window.location.href="/home"
+        },
+        error: (error : HttpErrorResponse) => {
+          this.alertService.error(error.error.error);
+          this.loading = false;
+      }});
+
     }
   }
 }
