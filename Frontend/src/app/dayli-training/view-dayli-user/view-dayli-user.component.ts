d48f417b7d129579b7b1d20b1bbc3b5aaae5132a
Moma1706 <milan.momcilovic582@gmail.com>
2023-05-29 20:13:41 +0200

    date fix
    date fix

--- Frontend/src/app/dayli-training/view-dayli-user/view-dayli-user.component.ts
+++ Frontend/src/app/dayli-training/view-dayli-user/view-dayli-user.component.ts
@@ -72,13 +72,17 @@ export class ViewDayliUserComponent implements OnInit {
       this.model.firstName = this.f['firstname'].value;
     }
     if(this.f['lastname'].value != ''){
-    this.model.lastName=this.f['lastname'].value;
+      this.model.lastName=this.f['lastname'].value;
     }
+    if(this.f['dateOfBirth'].value != ''){
+      this.model.dateOfBirth=this.f['dateOfBirth'].value;
+      }
     
     this.dayliService.update(this.model.id, this.model).subscribe((response:any) =>{
-      window.location.reload();
-      console.log(response);
-      this.alertservice.success("Profil korisnika promenjen!")
+      
+      const returnUrl ='/dayli-training/view-all-dayli';
+      this.router.navigateByUrl(returnUrl);
+      this.alertservice.success("Profil korisnika promenjen!");
     })
   }
 
